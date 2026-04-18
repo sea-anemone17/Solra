486de6ed315be24f3ae8216cf09a34926df8dc7c
sea-anemone17 <leehaeju09@gmail.com>
2026-04-18 21:13:55 +0900

    Update achievements.js

--- js/logic/achievements.js
+++ js/logic/achievements.js
@@ -1,1 +1,17 @@
+export function getAchievements(completeCount) {
+  const list = [];
 
+  if (completeCount >= 1) {
+    list.push("첫 해결");
+  }
+
+  if (completeCount >= 3) {
+    list.push("꾸준한 Solver");
+  }
+
+  if (completeCount >= 5) {
+    list.push("신뢰 축적");
+  }
+
+  return list;
+}
