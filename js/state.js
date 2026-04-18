ef5518ea8cb4b08ef206cf0bee4225bd8a05d8c8
sea-anemone17 <leehaeju09@gmail.com>
2026-04-18 20:49:21 +0900

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,18 @@
+export const state = {
+  currentPage: "home",
+  currentSubject: "수학",
+  currentTaskType: "",
+  selectedTags: ["고2"],
+  dmRequests: [],
+  selectedDmId: null,
+  notifications: [],
+  reviews: [],
+  profile: {
+    solverName: "Hazel",
+    bio: "이해가 되는 설명을 지향합니다.",
+    tags: ["고2 대상", "구조화", "시험 대비"],
+    level: 1,
+    xp: 0,
+    completeCount: 0
+  }
+};
