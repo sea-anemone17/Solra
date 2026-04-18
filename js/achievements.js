cbf4ef672320bcaf576adc7b4fb4e712748bbab7
sea-anemone17 <leehaeju09@gmail.com>
2026-04-18 20:48:57 +0900

    Delete js/achievements.js

--- js/achievements.js
+++
@@ -1,7 +0,0 @@
-import { achievements } from "./data.js";
-
-export function getUnlockedAchievements(state) {
-  return achievements.filter((achievement) => {
-    return !state.unlockedAchievementIds.has(achievement.id) && achievement.check(state);
-  });
-}
